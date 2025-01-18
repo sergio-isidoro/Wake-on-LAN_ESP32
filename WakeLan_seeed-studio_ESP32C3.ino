4591af229e726a85a3f934edb78330b705084cd5
Sergio Isidoro <70864023+manoper93@users.noreply.github.com>
2025-01-18 11:45:38 +0000

    Update WakeLan_seeed-studio_ESP32C3.ino

--- WakeLan_seeed-studio_ESP32C3.ino
+++ WakeLan_seeed-studio_ESP32C3.ino
@@ -2,22 +2,20 @@
 #include <WiFiUdp.h>
 
 // Wi-Fi network configurations
-const char* ssid = "XXXX";
-const char* password = "XXXX";
+const char* ssid = "wifi_name";
+const char* password = "wifi_pass";
 
 // MAC address of the device to be woken up
 const uint8_t macAddress[6] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF };
 
 // UDP configurations
 WiFiUDP udp;
-const char* broadcastIP = "XXX.XXX.XXX.XXX"; // Broadcast address
-const int udpPort = XXXX;
+const char* broadcastIP = "XXX.XXX.XXX.XXX"; // Replace with the broadcast address of the network
+const int udpPort = XXXX; // Standard port for Wake-on-LAN
 
-// Button GPIO
-const int interruptPin = GPIO_NUM_2; // GPIO2 (D0)
+#define BUTTON_PIN_BITMASK 0x200000000 // 2^33 in hex
 
-// Flag to signal the wake event
-volatile bool wake = false;
+RTC_DATA_ATTR int bootCount = 0; //save in memory in deep sleep, in case of reset ou poweroff default = 0
 
 // Function to send the Magic Packet
 void sendWakeOnLanPacket() {
@@ -26,44 +24,77 @@ void sendWakeOnLanPacket() {
   for (int i = 1; i <= 16; i++) {
     memcpy(&packet[i * 6], macAddress, 6); // Fill with the MAC address
   }
+
   udp.beginPacket(broadcastIP, udpPort);
   udp.write(packet, sizeof(packet));
   udp.endPacket();
-}
 
-// Interrupt function
-void IRAM_ATTR handleInterrupt() {
-  wake = true; // Signal the wake event
+  Serial.println("Magic Packet sent.");
 }
 
 // Connect to Wi-Fi
 void connectWiFi() {
   if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting to Wi-Fi...");
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
       delay(100);
+      Serial.print(".");
     }
+    Serial.println("\nWi-Fi connected.");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
     udp.begin(udpPort); // Reconfigure UDP after reconnection
   }
 }
 
-void setup() {
-  // Configure the interrupt pin
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, FALLING);
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
 
-  // Initialize Wi-Fi to be ready at startup
-  connectWiFi();
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
 }
 
-void loop() {
-  if (wake) {
-    wake = false; // Reset the wake flag
+void setup(){
+  Serial.begin(115200);
+  delay(1000); //Take some time to open up the Serial Monitor
 
-    // Send the Magic Packet
-    sendWakeOnLanPacket();
+  Serial.println("Boot number: " + String(bootCount));
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  esp_deep_sleep_enable_gpio_wakeup(BIT(D0), ESP_GPIO_WAKEUP_GPIO_LOW); //gpio2 D0
+
+  connectWiFi();
+
+  for (int i = 1; i <= 10; i++) {
+    Serial.print(", ");
+    Serial.println(i);
+    delay(1000);
+    //send 10x if is not reset board, only wakeup status
+    if(bootCount != 0) sendWakeOnLanPacket();
   }
+  
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  
+  //Go to sleep now
+  Serial.println(" ");
+  Serial.println("Going to sleep now");
+  esp_deep_sleep_start();
+}
 
-  // Add other logic here if necessary
-  delay(10); // Small delay to avoid an overly fast loop
+void loop(){
+  //This is not going to be called
 }
