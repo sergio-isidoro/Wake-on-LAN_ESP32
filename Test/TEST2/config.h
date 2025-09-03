85a312c0f9d02e51da666d750309e71eee0927fc
Sérgio Isidoro <70864023+sergio-isidoro@users.noreply.github.com>
2025-09-03 14:45:38 +0100

    Create config.h

---
+++ Test/TEST2/config.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <Arduino.h>
+#include <SPIFFS.h>
+#include <ArduinoJson.h>
+
+#define FIRMWARE_VERSION "5.0"
+#define RESET_BUTTON_PIN D2
+#define BUTTON_GPIO      D0
+#define LED_GPIO         D1
+#define OTA_CHECK_INTERVAL_MS 60000UL
+#define PING_DELAY_AFTER_WOL  60000UL
+
+struct Config {
+  char ssid[32];
+  char password[64];
+  char mqtt_server[64];
+  int  mqtt_port;
+  char mqtt_user[32];
+  char mqtt_password[64];
+  char target_ip[16];
+  char broadcastIPStr[16];
+  uint8_t mac_address[6];
+  int  udp_port;
+};
+
+extern Config config;
+extern unsigned long lastOTACheck;
+extern unsigned long wolSentAt;
+extern bool wolPendingPing;
+extern bool buttonTriggered;
+
+bool saveConfig(const Config &cfg);
+bool loadConfig();
+void factoryReset();
+void runSerialSetup();
