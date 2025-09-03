2cf2c74eb66bd6cd7b59f9cae1f6042603b63493
Sérgio Isidoro <70864023+sergio-isidoro@users.noreply.github.com>
2025-09-03 14:48:46 +0100

    Create helpers.cpp

---
+++ Test/TEST2/helpers.cpp
@@ -0,0 +1,27 @@
+#include "helpers.h"
+#include "mqtt.h"
+#include "config.h"
+
+WiFiUDP udp;
+
+void mqttPublish(const char* msg) {
+  Serial.println(msg);
+  if (mqtt.connected()) mqtt.publish("wol/log", msg);
+}
+
+void blinkDigit(int n) {
+  for (int i=0;i<n;i++) {
+    digitalWrite(LED_GPIO, HIGH); delay(100);
+    digitalWrite(LED_GPIO, LOW); delay(100);
+  }
+}
+
+void blinkVersion(const char* version) {
+  int len = strlen(version);
+  for (int i=0;i<len;i++) {
+    if (version[i]>='0' && version[i]<='9') {
+      blinkDigit(version[i]-'0');
+      delay(500);
+    }
+  }
+}
