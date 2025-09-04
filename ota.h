803fe99de274900dde699045c59db47901539112
Sérgio Isidoro <70864023+sergio-isidoro@users.noreply.github.com>
2025-09-04 09:53:43 +0100

    Update ota.h

--- ota.h
+++ ota.h
@@ -1,4 +1,10 @@
 #pragma once
+
 #include <Arduino.h>
 
+#define OTA_FILE_PATH   "/firmware.bin"
+#define OTA_SHA_PATH    "/firmware.sha256"
+#define MAX_OTA_RETRIES 3
+
 void performOTA();
+String calculateSHA256(const char* path);
